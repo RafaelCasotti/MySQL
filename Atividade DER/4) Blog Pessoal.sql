2102ffec1197125eee3146d4808f2d767467514c
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-02-15 13:55:26 -0300

    Atividades Blog Pessoal

---
+++ Atividade DER/4) Blog Pessoal.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE db_blogpessoal;
+
+USE db_blogpessoal;
+
+CREATE TABLE tb_temas(
+id BIGINT NOT NULL AUTO_INCREMENT,
+descricao VARCHAR(255) NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE tb_usuarios(
+id BIGINT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(255) NOT NULL,
+usuario VARCHAR(255) NOT NULL,
+senha VARCHAR(255) NOT NULL,
+foto VARCHAR(255),
+PRIMARY KEY(id)
+);
+
+
+CREATE TABLE tb_postagem(
+id BIGINT NOT NULL AUTO_INCREMENT,
+titulo VARCHAR(100) NOT NULL,
+texto VARCHAR(1000) NOT NULL,
+`data` DATE,
+tema_id BIGINT,
+usuario_id BIGINT,
+PRIMARY KEY(id),
+FOREIGN KEY (tema_id) REFERENCES tb_temas(id),
+FOREIGN KEY (usuario_id) REFERENCES tb_usuarios(id)
+);
